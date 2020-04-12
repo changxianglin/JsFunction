4cbf8b0be592dea9e81388bdcd368ed23dbe1d84
changxianglin <281532612@qq.com>
2020-04-13 01:10:04 +0800

    function use

---
+++ funcTransfer.js
@@ -0,0 +1,13 @@
+var myObject = {
+    value: 0,
+    increment: function (inc) {
+        this.value += typeof inc === 'number' ? inc : 1
+    }
+}
+
+myObject.increment()
+document.writeln(myObject.value)
+
+myObject.increment(2)
+document.writeln(myObject.value)
+
