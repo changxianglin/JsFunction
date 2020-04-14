a41dac698fc6640481b296bdc033815a22e9cb95
changxianglin <281532612@qq.com>
2020-04-15 00:22:08 +0800

    memoization

---
+++ memoization.js
@@ -0,0 +1,17 @@
+var momoizer = function (memo, formula) {
+    var recur = function (n) {
+        var result = memo[n]
+        if (typeof result !== 'number') {
+            result = formula (recur, n)
+            memo[n] = result
+        }
+        return result
+    }
+    return recur
+}
+
+var fibonacci = momoizer([0, 1], function (recur, n) {
+    return recur (n -1) + recur (n -2)
+})
+
+console.log(fibonacci(10))
